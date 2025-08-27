4061eacb50114dc09deda94c862a26830a52a832
kartikey sharma <kartikey7518@gmail.com>
2025-08-27 08:16:15 +0530

    created share feature and see shared content through link

---
+++ client/src/component/SharedContent.tsx
@@ -0,0 +1,54 @@
+import axios from "axios"
+import { BACKEND_URL } from "../utils/config"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
+const SharedContent = () => {
+
+    const [content, setContent] = useState([])
+    const [showExpired, setShowExpired] = useState(false)
+
+    const {shareLink} = useParams();
+
+    const fetchSharedContent = async() => {
+        const res = await axios.get(BACKEND_URL + "/user/link/" + shareLink, {
+            withCredentials: true
+        }  )
+
+        if(res.data === "link is expired"){
+            setShowExpired(true)
+        }
+        setContent(res?.data)
+    }
+
+    useEffect(() => {
+        fetchSharedContent()
+    }, [])
+
+    if (showExpired) {
+    return <div className="flex justify-center pt-10 text-gray-600">Link Expired!!</div>;
+    }
+
+    if(content.length === 0){
+        return <div className="flex justify-center pt-20">
+                <h1 className="text-2xl text-gray-600">No Content Exists!!!</h1>
+            </div>
+    }
+
+  return (
+    <div>
+        <div className="flex justify-center pt-5 text-xl">
+            <h1 className="">Shared Content by <span className="font-semibold text-gray-600
+            ">{content[0]?.userId?.username}</span></h1>
+        </div>
+
+        <div className="flex flex-wrap gap-4 p-4">
+            {content.map((item: any) => <div key={item._id}><Card type={item.type} title={item.title} link={item.link} id={item._id} /></div> )}
+            
+        </div>
+    </div>
+  )
+}
+
+export default SharedContent
--- client/src/pages/DashBoard.tsx
+++ client/src/pages/DashBoard.tsx
@@ -5,10 +5,24 @@ import Sidebar from "../component/Sidebar"
 import AddIcons from "../icon/icons"
 import { ShareIcon } from "../icon/icons"
 import Content from "../component/Content"
+import axios from "axios"
+import { BACKEND_URL } from "../utils/config"
 
 function DashBoard() {
 
   const [showAdd, setShowAdd] = useState(false)
+
+  const handleShare = async () => {
+    const confirmation = window.confirm("Do you want on sharing??")
+
+    const res = await axios.post(BACKEND_URL + "/user/link", {
+      share: confirmation ? true : false
+    }, {
+      withCredentials: true
+    })
+
+    alert(res.data.hash ? `http://localhost:5173/link/${res.data.hash}` : res.data)
+  }
 
   return (
     <div className="flex" >
@@ -17,24 +31,41 @@ function DashBoard() {
 
   <div className="w-full bg-gray-200 p-2 min-h-screen">
     <div className="flex justify-between items-center">
-        <p className=" ml-5 font-semibold">All Notes</p>
+        <p className="ml-2 sm:ml-5 font-semibold">All Notes</p>
 
-        <div className="flex gap-3 items-center justify-end">
+      <div className="sm:hidden flex gap-3 items-center justify-end">
 
-          <Button variant="secondary"
-          startIcon={<ShareIcon size="sm"/>}
-          size="md" onclick={() => {}} text="Share Brain" />
+          <div onClick={handleShare}>
+            <Button variant="secondary"
+            startIcon={<ShareIcon size="sm"/>}
+            size="sm" onclick={() => {}} text="Share Brain" />
+          </div>
+
+          <div onClick={() => setShowAdd(true)}>
+            <Button variant="primary" size="sm" onclick={() => {}} text="Add Content"
+              startIcon={<AddIcons size= "sm" />}
+              />
+          </div>
+      </div>
+
+        <div className="hidden sm:flex gap-3 items-center justify-end">
+          <div onClick={handleShare}>
+            <Button variant="secondary"
+            startIcon={<ShareIcon size="md"/>}
+            size="md" onclick={() => {}} text="Share Brain" />
+          </div>
 
         <div onClick={() => setShowAdd(true)}>
           <Button variant="primary" size="md" onclick={() => {}} text="Add Content"
-            startIcon={<AddIcons size= "sm" />}
+            startIcon={<AddIcons size= "md" />}
             />
         </div>
       </div>
+
     </div>
 
 
-      <div className="flex gap-2 flex-wrap">
+      <div className="flex justify-center gap-2 flex-wrap">
         <Content />
       </div>
       </div>
