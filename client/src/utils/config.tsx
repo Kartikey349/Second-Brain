8b195899a6b7d97951fe23d0750cc564bc355042
Kartikey Sharma <122649866+Kartikey349@users.noreply.github.com>
2025-08-27 22:58:19 +0530

    Update config.tsx

--- client/src/utils/config.tsx
+++ client/src/utils/config.tsx
@@ -1,1 +1,1 @@
-export const BACKEND_URL = window.location.hostname === "localhost" ? "http://localhost:3001" : "https://second-brain-backend-r6y3.onrender.com/"
+export const BACKEND_URL = window.location.hostname === "localhost" ? "http://localhost:3001" : "https://second-brain-backend-r6y3.onrender.com"
